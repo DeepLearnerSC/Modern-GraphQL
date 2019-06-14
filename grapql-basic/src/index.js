5795aa3bdb03959105bba8d6b7281b397fdc4a7a
Song Cho <deeplearner507@gmail.com>
2019-06-15 00:45:05 +0900

    graphql Scalar Types

--- grapql-basic/src/index.js
+++ grapql-basic/src/index.js
@@ -1,6 +1,44 @@
-console.log('Hello GraphQL')
+import { GraphQLServer } from 'graphql-yoga'
 
-import myAddFunction, { subtract } from './math'
+// Scalar types - String, Boolean, Int, Float, ID
 
-console.log(myAddFunction(1, -2))
-console.log(subtract(10, 2))
+// Type definitions (schema)
+const typeDefs = `
+    type Query {
+        title: String!
+        price: Float!
+        releaseYear: Int
+        rating: Float
+        isStock: Boolean!
+    }
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        title() {
+            return 'The War of Art'
+        },
+        price() {
+            return 12.99
+        },
+        releaseYear() {
+            return null
+        },
+        rating() {
+            return 5
+        },
+        isStock() {
+            return true
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('The server is up!')
+})
