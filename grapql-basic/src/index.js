95582e4f052070d84cbe8cd7a6552336de9b7896
Song Cho <deeplearner507@gmail.com>
2019-06-17 00:21:31 +0900

    relational arrays

--- grapql-basic/src/index.js
+++ grapql-basic/src/index.js
@@ -22,41 +22,46 @@ const posts = [{
     id: '10',
     title: 'GraphQL 101',
     body: 'This is how to use GraphQL...',
-    published: true
+    published: true,
+    author: '1'
 }, {
     id: '11',
     title: 'GraphQL 201',
     body: 'This is an advanced GraphQL post...',
-    published: false
+    published: false,
+    author: '1'
 }, {
     id: '12',
     title: 'Programming Music',
     body: '',
-    published: false
+    published: false,
+    author: '2'
 }]
 
 // Type definitions (schema)
 const typeDefs = `
-type Query {
-    users(query: String): [User!]!
-    posts(query: String): [Post!]!
-    me: User!
-    post: Post!
-}
+    type Query {
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
+        me: User!
+        post: Post!
+    }
 
-type User {
-    id: ID!
-    name: String!
-    email: String!
-    age: Int
-}
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+        posts: [Post!]!
+    }
 
-type Post {
-    id: ID!
-    title: String!
-    body: String!
-    published: Boolean!
-}
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+        author: User!
+    }
 `
 
 // Resolvers
@@ -96,6 +101,20 @@ const resolvers = {
                 body: '',
                 published: false
             }
+        }
+    },
+    Post: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        }
+    },
+    User: {
+        posts(parent, args, ctx, info) {
+            return posts.filter((post) => {
+                return post.author === parent.id
+            })
         }
     }
 }
