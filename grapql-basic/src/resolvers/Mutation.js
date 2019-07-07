9cd21758bf70bd05ec2208e5243f8024d938daa1
Song Cho <deeplearner507@gmail.com>
2019-07-07 20:01:46 +0900

    post sub done

--- grapql-basic/src/resolvers/Mutation.js
+++ grapql-basic/src/resolvers/Mutation.js
@@ -70,7 +70,7 @@ const Mutation = {
 
         return user
     },
-    createPost(parent, args, { db }, info) {
+    createPost(parent, args, { db, pubsub }, info) {
         const userExists = db.users.some((user) => user.id === args.data.author)
 
         if (!userExists) {
@@ -87,6 +87,10 @@ const Mutation = {
         }
 
         db.posts.push(post)
+
+        if (args.data.published) {
+            pubsub.publish('post', { post })
+        }
 
         return post
     },
