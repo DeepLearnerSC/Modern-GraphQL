e4653a3f7a399b63b605fcb07cda94a87f59baf8
Song Cho <deeplearner507@gmail.com>
2019-08-20 23:42:10 +0900

    add sorting feature

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -1,31 +1,23 @@
+import getUserId from '../utils/getUserId'
+
 const Query = {
-    // users(parent, args, { db }, info) {
     users(parent, args, { prisma }, info) {
         const opArgs = {
             first: args.first,
             skip: args.skip,
-            after: args.after
+            after: args.after,
+            orderBy: args.orderBy
         }
         
         if (args.query) {
             opArgs.where = {
                 OR: [{
                     name_contains: args.query
-                }, {
-                    email_contains: args.query
                 }]
             }
         }
 
         return prisma.query.users(opArgs, info)
-
-        // if (!args.query) {
-        //     return db.users
-        // }
-
-        // return db.users.filter((user) => {
-        //     return user.name.toLowerCase().includes(args.query.toLowerCase())
-        // })
     },
     myPosts(parent, args, { prisma, request }, info) {
         const userId = getUserId(request)
@@ -33,6 +25,7 @@ const Query = {
             first: args.first,
             skip: args.skip,
             after: args.after,
+            orderBy: args.orderBy,
             where: {
                 author: {
                     id: userId
@@ -50,12 +43,12 @@ const Query = {
 
         return prisma.query.posts(opArgs, info)
     },
-    // posts(parent, args, { db }, info) {
     posts(parent, args, { prisma }, info) {
         const opArgs = {
             first: args.first,
             skip: args.skip,
             after: args.after,
+            orderBy: args.orderBy,
             where: {
                 published: true
             }
@@ -75,7 +68,8 @@ const Query = {
         const opArgs = {
             first: args.first,
             skip: args.skip,
-            after: args.after
+            after: args.after,
+            orderBy: args.orderBy
         }
 
         return prisma.query.comments(opArgs, info)
